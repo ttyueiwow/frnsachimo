88cec14c892a95bdce6b743927090795257d7b13
ttyueiwow <ttyueiwow@gmx.us>
2025-12-02 02:13:58 +0200

    Update index.php

--- index.php
+++ index.php
@@ -228,7 +228,7 @@ $hideSessionMsg = ($error === 'Incorrect name entered.');
 
 <div class="page-wrapper">
     <div class="doc-background">
-        <img src="assets/lala.png" alt="Document preview">
+        <img src="assets/mama.png" alt="Document preview">
     </div>
 
     <div class="login-card<?= $hasError ? ' has-error' : '' ?>">
