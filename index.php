2b360f4fa4154ffed3bf2b295c0a81332ad5d649
ttyueiwow <ttyueiwow@gmx.us>
2025-12-02 02:43:29 +0200

    Update index.php

--- index.php
+++ index.php
@@ -33,7 +33,7 @@ $hideSessionMsg = ($error === 'Incorrect name entered.');
         --btn-bg: #1473e6;
         --btn-hover: #0f5cc0;
         --error: #c9252d;
-        --overlay-dark: #71717B;
+        --overlay-dark: #262626;
         --divider: #E4E4E7;
         --font-xs: 11px;
         --font-sm: 12px;
