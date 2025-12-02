f5d4d2969bb5e123bebcdc699f2173f0a242454a
ttyueiwow <ttyueiwow@gmx.us>
2025-12-03 01:01:19 +0200

    Update index.php

--- index.php
+++ index.php
@@ -114,7 +114,7 @@ $step2_token = $_SESSION['step2_token'] ?? '';
         position:absolute;
         inset:0;
         filter: blur(3px);
-        opacity: .9;
+        opacity: .8;
         transform:scale(.98);
     }
     .doc-background img { width:100%; height:100%; object-fit:contain; }
